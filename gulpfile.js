2b50e62dc984f04a822227d26be2acc906da3d09
Nikita Pevnev <n.pevnev@pixelplex.io>
2020-10-29 15:04:23 +0300

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -1,99 +1,78 @@
-// Определяем константы Gulp
 const { src, dest, parallel, series, watch } = require('gulp');
-// Подключаем Browsersync
 const browserSync = require('browser-sync').create();
-// Подключаем gulp-concat
 const concat = require('gulp-concat');
-// Подключаем gulp-uglify-es
 const uglify = require('gulp-uglify-es').default;
-// Подключаем модули gulp-sass и gulp-less
 const sass = require('gulp-sass');
-// Подключаем Autoprefixer
 const autoprefixer = require('gulp-autoprefixer');
-// Подключаем модуль gulp-clean-css
 const cleancss = require('gulp-clean-css');
-// Подключаем gulp-imagemin для работы с изображениями
 const imagemin = require('gulp-imagemin');
-// Подключаем модуль del
 const del = require('del');
   
-// Определяем логику работы Browsersync
 function browsersync() {
-  browserSync.init({ // Инициализация Browsersync
-    server: { baseDir: 'app/' }, // Указываем папку сервера
-    notify: false, // Отключаем уведомления
-    online: true // Режим работы: true или false
+  browserSync.init({
+    server: { baseDir: 'app/' },
+    notify: false,
+    online: true
   })
 }
   
 function scripts() {
-  return src([ // Берём файлы из источников
-    'node_modules/jquery/dist/jquery.min.js', // Пример подключения библиотеки
-    'app/js/app.js', // Пользовательские скрипты, использующие библиотеку, должны быть подключены в конце
+  return src([
+    'node_modules/jquery/dist/jquery.min.js',
+    'app/js/app.js',
     ])
-  .pipe(concat('app.min.js')) // Конкатенируем в один файл
-  .pipe(uglify()) // Сжимаем JavaScript
-  .pipe(dest('app/js/')) // Выгружаем готовый файл в папку назначения
-  .pipe(browserSync.stream()) // Триггерим Browsersync для обновления страницы
+  .pipe(concat('app.min.js'))
+  .pipe(uglify())
+  .pipe(dest('app/js/'))
+  .pipe(browserSync.stream())
 }
   
 function styles() {
-  return src('app/scss/style.scss') // Выбираем источник: "app/sass/main.sass" или "app/less/main.less"
-  .pipe(sass()) // Выбе
-  .pipe(concat('style.min.css')) // Конкатенируем в файл style.min.js
-  .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true })) // Создадим префиксы с помощью Autoprefixer
-  .pipe(cleancss( { level: { 1: { specialComments: 0 } } } )) // Минифицируем стили
-  .pipe(dest('app/css/')) // Выгрузим результат в папку "app/css/"
-  .pipe(browserSync.stream()) // Сделаем инъекцию в браузер
+  return src('app/scss/style.scss')
+  .pipe(sass())
+  .pipe(concat('style.min.css'))
+  .pipe(autoprefixer({ overrideBrowserslist: ['last 10 versions'], grid: true }))
+  .pipe(cleancss( { level: { 1: { specialComments: 0 } } } ))
+  .pipe(dest('app/css/'))
+  .pipe(browserSync.stream())
 }
   
 function images() {
-  return src('app/images/**/*') // Берём все изображения из папки источника
-  .pipe(imagemin()) // Сжимаем и оптимизируем изображеня
-  .pipe(dest('app/images/')) // Выгружаем оптимизированные изображения в папку назначения
+  return src('app/images/**/*')
+  .pipe(imagemin())
+  .pipe(dest('app/images/'))
 }
 
 function cleanimg() {
-  return del('app/images/**/*', { force: true }) // Удаляем всё содержимое папки "app/images/"
+  return del('app/images/**/*', { force: true })
 }
   
 function buildcopy() {
-  return src([ // Выбираем нужные файлы
+  return src([
     'app/css/**/*.min.css',
     'app/js/**/*.min.js',
     'app/images/**/*',
     'app/**/*.html',
     'app/fonts/**/*'
-    ], { base: 'app' }) // Параметр "base" сохраняет структуру проекта при копировании
-  .pipe(dest('dist')) // Выгружаем в папку с финальной сборкой
+    ], { base: 'app' })
+  .pipe(dest('dist'))
 }
   
 function cleandist() {
-  return del('dist/**/*', { force: true }) // Удаляем всё содержимое папки "dist/"
+  return del('dist/**/*', { force: true })
 }
   
 function startwatch() {
-  // Выбираем все файлы JS в проекте, а затем исключим с суффиксом .min.js
   watch(['app/**/*.js', '!app/**/*.min.js'], scripts);
-  // Мониторим файлы препроцессора на изменения
   watch('app/**/scss/**/*', styles);
-  // Мониторим файлы HTML на изменения
   watch('app/**/*.html').on('change', browserSync.reload);
-  // Мониторим папку-источник изображений и выполняем images(), если есть изменения
   watch('app/images/**/*', images);
 }
   
-// Экспортируем функцию browsersync() как таск browsersync. Значение после знака = это имеющаяся функция.
 exports.browsersync = browsersync;
-// Экспортируем функцию scripts() в таск scripts
 exports.scripts = scripts;
-// Экспортируем функцию styles() в таск styles
 exports.styles = styles;
-// Экспорт функции images() в таск images
 exports.images = images;
-// Экспортируем функцию cleanimg() как таск cleanimg
 exports.cleanimg = cleanimg;
-// Создаём новый таск "build", который последовательно выполняет нужные операции
 exports.build = series(cleandist, styles, scripts, buildcopy);
-// Экспортируем дефолтный таск с нужным набором функций
 exports.default = parallel(styles, scripts, browsersync, startwatch);
